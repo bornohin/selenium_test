2e85c54c667016ff45f09c1579a34da76dd1338f
bornohin <tonmoyenator@gmail.com>
2020-11-01 22:24:25 -0800

    Updated

--- test.py
+++ test.py
@@ -6,6 +6,7 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
 # Locate local copy of chromedriver on win 10.
+# PATH = "/home/mrana/Codes_And_Experiments/DataScienceUtilityProjects/chromedriver"
 PATH = "C:\Program Files (x86)\chromedriver\chromedriver.exe"
 
 driver = webdriver.Chrome(PATH)  # Wont work without this locally
@@ -24,23 +25,27 @@ finally:
 # Lets try something different this time.
 driver.get("https://www.tomislam.com/blog/")  # Pointing at the blog section.
 
-
 try:
     blog = WebDriverWait(driver, 10).until(
         EC.presence_of_element_located((By.CSS_SELECTOR, "div.col-lg-8"))  # Easy to find by ID and then manipulate inside it.
     )
-    posts = blog.find_elements_by_xpath('//*[@id="about"]/div[3]/div/div/div[2]/div[1]/div[1]/div/h2')
+    posts = blog.find_elements_by_tag_name("h2")
     dates = blog.find_elements_by_css_selector("p.card-text")
 
-    for i in range(len(dates)):
-        print(dates[i].text)
+    print(len(dates))
+    print("0", dates[0].text)
+    print("1", dates[1].text)
+    print("2", dates[2].text)
+    print("3", dates[3].text)
+
+    # for date in dates:
+    #     print(date.text)
 
     print(f"Total {len(posts)} posts found")
 
     for i in range(len(posts)):
-        print(f"Post number {i+1}: ", posts[i].text, "by", dates[i].text)
-        print("*******", dates[1].text)
+        print(f"Post number {i+1}: ", posts[i].text, "by", dates[i*2].text)
 finally:
     driver.quit()
 
-# The output is, it can show how many articles are there in BLOG page along with name of blogger and date.
+# By xpath
